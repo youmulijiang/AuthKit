cd2056c21b71ca5b5c302dcb5d95d5f9bcef01b4
youmulijiang <youmulijiang@163.com>
2026-03-05 10:43:12 +0800

    feat:初始化项目环境

---
+++ src/main/java/AuthKit.java
@@ -0,0 +1,9 @@
+import burp.api.montoya.BurpExtension;
+import burp.api.montoya.MontoyaApi;
+
+public class AuthKit implements BurpExtension {
+    @Override
+    public void initialize(MontoyaApi montoyaApi) {
+
+    }
+}
